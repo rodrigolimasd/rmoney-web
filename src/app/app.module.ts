6d474838a15893212f726c84a998e488f5d3f6ab
Rodrigo Lima <rodrigolima>
2018-04-16 20:58:03 -0300

    implementado module core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { CoreModule } from './core/core.module';
+
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
 
+    CoreModule,
     LancamentosModule,
     PessoasModule,
 
