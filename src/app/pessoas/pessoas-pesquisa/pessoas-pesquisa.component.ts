9ddab51acacb01195169d564dd9251bac7e25e9b
Rodrigo Lima <rodrigolima>
2018-04-30 14:59:00 -0300

    implementação da exclusão de pessoa

--- src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -1,6 +1,9 @@
+import { ToastyService } from 'ng2-toasty';
+import { ErrorHandlerService } from './../../core/error-handler.service';
+import { ConfirmationService } from 'primeng/components/common/api';
 import { LazyLoadEvent } from 'primeng/components/common/lazyloadevent';
 import { PessoaFiltro, PessoaService } from './../pessoa.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-pessoas-pesquisa',
@@ -12,8 +15,14 @@ export class PessoasPesquisaComponent {
   totalRegistros = 0;
   filtro = new PessoaFiltro();
   pessoas = [];
+  @ViewChild('tabela') grid;
 
-  constructor(private pessoaService: PessoaService) { }
+  constructor(
+    private pessoaService: PessoaService,
+    private toasty: ToastyService,
+    private confirmation: ConfirmationService,
+    private errorHandler: ErrorHandlerService
+  ) { }
 
   pesquisar(pagina = 0) {
     this.filtro.pagina = pagina;
@@ -27,6 +36,27 @@ export class PessoasPesquisaComponent {
   aoMudarPagina(event: LazyLoadEvent) {
     const pagina = event.first / event.rows;
     this.pesquisar(pagina);
+  }
+
+  confirmarExclusao(pessoa: any) {
+    this.confirmation.confirm({
+      message: 'Deseja realmente excluir?',
+      accept: () => {
+        this.excluir(pessoa);
+      }
+    });
+  }
+
+  excluir(pessoa: any) {
+    this.pessoaService.excluir(pessoa.codigo)
+        .then(() => {
+          if (this.grid.first === 0) {
+            this.pesquisar();
+          } else {
+            this.grid.first = 0;
+          }
+          this.toasty.success('Pessoa excluída com sucesso!');
+        }).catch(erro =>  this.errorHandler.handle(erro));
   }
 
 }
