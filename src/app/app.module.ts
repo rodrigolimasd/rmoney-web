220bac4d292bce2eaf403f43589dfeb6ff581b24
Rodrigo Lima <rodrigolima>
2018-04-30 16:56:47 -0300

    usando roteamento básico na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
+import { PessoaCadastroComponent } from './pessoas/pessoa-cadastro/pessoa-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { ConfirmationService } from 'primeng/components/common/api';
 
@@ -12,6 +16,12 @@ import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent},
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+  { path: 'pessoas', component: PessoasPesquisaComponent},
+  { path: 'pessoas/nova', component: PessoaCadastroComponent}
+];
 
 @NgModule({
   declarations: [
@@ -23,6 +33,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
+    RouterModule.forRoot(routes),
     LancamentosModule,
     PessoasModule
   ],
